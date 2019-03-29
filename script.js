28c9efff4c85cfb623b12868865712cd5cd6bc8e
Joshua Ku <joshuamku9@gmail.com>
2019-03-29 14:46:29 -0500

    Add clear button

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
-const container = document.querySelector("div");
-for(let i = 0; i < 8; ++i){
-    for(let j = 0; j < 8; ++j){
-        const grid = document.createElement("div");
-        grid.setAttribute('class','box');
-        container.appendChild(grid);
+
+const container = document.querySelector('.container');
+
+function createGrid(number){
+    for(let i = 0; i < number; ++i){
+        for(let j = 0; j < number; ++j){
+            const grid = document.createElement("div");
+            grid.setAttribute('class','box');
+            container.appendChild(grid);
+        }
     }
+    onHover();
 }
 
 function onHover(){
@@ -15,4 +20,20 @@ function onHover(){
         })
     })
 }
+createGrid(8);
 onHover();
+const clearButton=document.querySelector('#clearBtn');
+
+clearButton.addEventListener('click',(e)=>{
+   let num=window.prompt("Enter desired number for the grid");
+   if(isNaN(num) || num==null || num== "" || num.trim() == "")
+   {
+       alert("You didn't enter a number. Please enter number")
+   }
+   else{
+        while(container.hasChildNodes()){
+            container.removeChild(container.firstChild);
+        }
+        createGrid(num);  
+    } 
+})
